f3a209e918c973225af5891a36747b95b9b18a5e
unknown <anishag629@gmail.com>
2024-12-04 23:08:21 +0530

    backend

--- travel-itinerary-backend/routes/auth.js
+++ travel-itinerary-backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { register, login } = require('../controllers/usercontroller');
 const router = express.Router();
-
+//anisha
 router.post("/register", register);
 router.post("/login", login);
 
